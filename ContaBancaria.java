777cc882fa9c0e8a5d64aa3c91e44e7e39b58595
Jailson Neiva <86751553+jailsonneiva@users.noreply.github.com>
2022-07-22 04:23:22 +0000

    Mudado método depositar para depósito Acrescentado método depositar.

--- ContaBancaria.java
+++ ContaBancaria.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
+
 class ContaBancaria {
-    Scanner sc = new Scanner(System.in);
-    
+    Scanner sc = new Scanner(System.in);    
     private double saldo = 10.0;
     protected double valor;
     public int opc;
@@ -14,26 +14,45 @@ class ContaBancaria {
     public String f = "Sacar valor";
     
 //DEPÓSITOS.....................................................
-    public Double depositar(){        
-        System.out.printf("DIGITE O VALOR DE DEPÓSITO -> R$ ");
-        valor = sc.nextDouble();
-        return saldo+=valor;
-    }
+  public Double deposito(){     
+    return saldo+=valor;
+  }
 
 //SAQUE/ DÉBITO..................................................
-     public Double debito() {
-        return saldo-=valor;
+  public Double debito() {
+    return saldo-=valor;
   }
     
 //GET - CONTA RECEBE O SALDO...................................... 
-    public Double getSaldo() {
-        return saldo;
+  public Double getSaldo() {
+    return saldo;
     } 
 
 //SET - INSERINDO O SALDO.........................................
-    public void setSaldo(double saldo) {
-        this.saldo=saldo;
+  public void setSaldo(double saldo) {
+    this.saldo=saldo;
+    }
+  
+//OPÇÃO DEPOSITAR.................................................
+  public void depositar(){
+    System.out.printf("DIGITE O VALOR DE DEPÓSITO -> R$ ");
+    valor = sc.nextDouble(); 
+   do{
+      if(valor==0){
+        System.out.printf(".................................%n('_') Você não digitou o valor.%n.................................%n");
+        System.out.printf("DIGITE O VALOR DE DEPÓSITO -> R$ ");
+        valor = sc.nextDouble(); 
+      }else if(valor<0){        
+        System.out.printf(".................................%n('_') Número Inválido. Digite um valor aceito:%n.................................%n");
+        System.out.printf("DIGITE O VALOR DE DEPÓSITO -> R$ ");       
+        valor = sc.nextDouble();
+      }      
+    }while(valor==0||valor<0);
+    if(valor>0){
+        deposito();
+        avisoDeposito();
     } 
+  }
   
 //OPÇÃO SAQUE-DÉBITO.............................................
   public void debitando(){     
@@ -42,27 +61,26 @@ class ContaBancaria {
     do{
       if(valor==0){
         System.out.printf(".................................%n('_') Você não digitou o valor.%n.................................%n");
-        System.out.printf("DIGITE O VALOR DO SAQUE -> R$ ");
-        valor = sc.nextDouble();
-      }else if(valor<0){
+        System.out.printf("DIGITE O VALOR DO SAQUE -> R$ "); 
+      }else if(valor<0){        
         System.out.printf(".................................%n('_') Número Inválido. Digite um valor aceito:%n.................................%n");
         System.out.printf("DIGITE O VALOR DO SAQUE -> R$ ");         
         valor = sc.nextDouble();
-      }
+      }      
     }while(valor==0||valor<0);
-    if(valor>saldo){
-        System.out.printf("............................%n('_') Saldo insuficiente.%n............................%n");  
-        opc4e5(); 
+    if(valor>saldo){        
+      System.out.printf("............................%n('_') Saldo insuficiente.%n............................%n");  
+        opc4e5();      
     }else if(valor>0 && valor<=saldo){
         debito();
         avisoSaque();
         opc4e5();
-    }
+    } 
   }
   
 //OPÇÃO SALDO - EXTRATO.........................................
     public void extrato(){
-        System.out.printf(".......................... %n('_') Saldo atualizado -> R$ %.2f%n.......................... ",getSaldo());   
+        System.out.printf(".......................... %n('_') Saldo atual -> R$ %.2f%n.......................... ",getSaldo());   
     } 
 
 
@@ -76,24 +94,16 @@ class ContaBancaria {
      System.out.print(" -> ");
      opc = sc.nextInt();
     }
-    if(opc==5){
-      opc5();
+    if(opc==4){
+      escolha();       
     }else{
-      escolha();   
+      opc5();
     }
   }
-  
-//OPÇÃO - ENCERRAMENTO PROGRAMA.....................................
-  public void opc5(){
-    System.out.printf("%n  ........: SISTEMA ENCERRADO :........%n");
-    System.out.println("-> Para usar novamente clique em 'Run' <-");
-    System.out.println("----------------------------------------");
-    sc.close();   
-  }
-  
-//OPÇÃO - VOLTAR - ENCERRAR - SACAR.................................. 
+
+  //OPÇÃO - VOLTAR - ENCERRAR - SACAR.................................. 
   public void opc4_5_6(){
-     System.out.printf("%n * 4 - %s%n * 5 - %s%n * 6 - %s%n",d,e,f);
+    System.out.printf("%n * 4 - %s%n * 5 - %s%n * 6 - %s%n",d,e,f);
        int opc = sc.nextInt();
        while(opc!=4 && opc!=5 && opc!=6){
      System.out.printf(".................................%n-> ERRO..escolha uma opção abaixo:%n.................................%n");
@@ -101,15 +111,21 @@ class ContaBancaria {
      System.out.print(" -> ");
      opc = sc.nextInt();
     }
-  if(opc==4){
-    escolha(); 
-       
-  }else if(opc==5){
-    opc5();
+    if(opc==4){
+      escolha();
+    }else if(opc==6){
+      debitando();      
   }else{
-    debitando();
-    opc4e5(); 
-    }
+    opc5();
+  }
+} 
+  
+//OPÇÃO - ENCERRAMENTO PROGRAMA.....................................
+  public void opc5(){
+    System.out.printf("%n ........: SISTEMA ENCERRADO :........%n");
+    System.out.println("-> Para usar novamente clique em 'Run' <-");
+    System.out.println("----------------------------------------");
+    sc.close();   
   }
 
 //AVISO DEPÓSITO...............................................
@@ -121,15 +137,14 @@ class ContaBancaria {
   public void avisoSaque(){
     System.out.printf("............................. %n-> Saque de R$ %.2f realizado%n.............................",valor);    
   }
-
- //...............MENU PRINCIPAL..................................
+ //...............MENU PRINCIPAL ESCOLHA-1..................................
   public void escolha(){
         System.out.printf("%nESCOLHA UMA OPÇÃO -> [1 2 3 5]:%n");
           System.out.printf(" * 1 - %s%n * 2 - %s%n * 3 - %s%n * 5 - %s%n",a,b,c,e);
           int opc = sc.nextInt();
-          while(opc!=1 && opc!=2 && opc!=3&& opc!=5){
+          while(opc!=1 && opc!=2 && opc!=3 && opc!=5){
              System.out.printf("...................................%n-> # ERRO # <-%nSelecione o ítem correto:%n...................................%n");
-          System.out.printf("%nESCOLHA UMA OPÇÃO -> [1 2 3 4 5]:%n");
+          System.out.printf("%nESCOLHA UMA OPÇÃO -> [1 2 3 5]:%n");
           System.out.printf(" * 1 - %s%n * 2 - %s%n * 3 - %s%n * 5 - %s%n",a,b,c,e);
              opc = sc.nextInt();
           }          
